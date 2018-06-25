99d5a01e33d4adce092ff9d75b035134184ee25a
merlin yang <479941812@qq.com>
2018-06-25 17:23:14 +0800

    add book model and user model

---
+++ Book_Management_System/Book_Management_System/Models/Book.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Management_System.Models
+{
+    class Book
+    {
+        private int _id;
+        public int id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        private string _name;
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        private string _author;
+        public string author
+        {
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                _author = value;
+            }
+        }
+
+        private string _publisher;
+        public string publisher
+        {
+            get
+            {
+                return _publisher;
+            }
+            set
+            {
+                _publisher = value;
+            }
+        }
+
+        private string _isbn;
+        public string isbn
+        {
+            get
+            {
+                return _isbn;
+            }
+            set
+            {
+                _isbn = value;
+            }
+        }
+
+        private string _description;
+        public string description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        private bool _status;
+        public bool status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+            }
+        }
+    }
+}
